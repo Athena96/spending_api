48b5c7f9670a20199002b960a1c9051c7adb6869
Athena96 <jaredfranzone@gmail.com>
2018-12-07 19:59:35 +0000

    spending_api: added endpoint to get month and year aggregated spending per category

--- flask_app.py
+++ flask_app.py
@@ -105,23 +105,39 @@ def delete_purchase(purchase_id):
     return jsonify({'result': 'successfuly deleted purchase!'})
 
 
-    #     '''
-#     A function that handles our DELETE API Endpoint.
-#     More specificaly, the function is used to delete a tuple in our database.
-#
-#     - Parameters:
-#     - cid: the cid for the tuple to be deleted
-#
-#     return: status msg
-#     '''
-#     print("deleteRecord()")
-#
-#     # query the DB
-#     cursor.execute("DELETE FROM StockData WHERE ticker = '{0}' AND date = DATE '{1}';".format(ticker, date))
-#
-#     # mariadb_connection.commit()
-#
-#     return jsonify({'result': 'deleted'})
+@app.route('/<string:month>/<string:year>', methods=['GET'])
+def get_spending_report(month, year):
+    print("get_spending_report() for: Y: {0} M: {1}".format(month, year))
+
+    # open
+    db_conn = sqlite3.connect(DATABASE)
+    cursor = db_conn.cursor()
+
+    # query
+    begin_date = "{0}-{1}-01 00:00:00".format(year, month)
+    end_date = "{0}-{1}-31 00:00:00".format(year, month)
+
+    # get list of categories
+    categories = {}
+    cursor.execute("SELECT distinct(spending.category) from spending")
+    for category in cursor:
+        print("->: ", category[0])
+        categories[category[0]] = 0.0
+
+    # get month and year purchase data
+    cursor.execute("SELECT spending.price, spending.category FROM spending where spending.date >= '{0}' and spending.date <= '{1}'".format(begin_date, end_date))
+
+    # aggregate
+    for price, category in cursor:
+        categories[category] += float(price)
+
+    # close
+    db_conn.close()
+
+    # send data
+    return jsonify(categories)
+
+
 
 # Update - PUT
 # @app.route('/<string:oldTicker>/<string:oldDate>/<string:ticker>/<string:openPrice>/<string:closePrice>/<string:highPrice>/<string:lowPrice>/<string:volume>/<string:date>', methods=['PUT'])
@@ -133,61 +149,3 @@ def delete_purchase(purchase_id):
 #     # mariadb_connection.commit()
 #
 #     return jsonify({'result': 'updated'})
-
-
-# Insert - POST
-# @app.route('/<string:ticker>/<string:openPrice>/<string:closePrice>/<string:highPrice>/<string:lowPrice>/<string:volume>/<string:date>', methods=['POST'])
-# def insertRecord(ticker, openPrice, closePrice, highPrice, lowPrice, volume, date):
-#     # start transaction
-#     #cursor.execute("START TRANSACTION;")
-#
-#     #savepointID = "insertSavepoint{0}".format(len(savepoints))
-#     #savepoints.append( savepointID )
-#
-#     #cursor.execute("SAVEPOINT {0}".format(savepointID))
-#
-#     # alter table
-#     # query the DB
-#     cursor.execute("INSERT INTO StockData (ticker, openPrice, closePrice, highPrice, lowPrice, volume, date) VALUES ('{0}', {1}, {2},{3},{4},{5}, DATE '{6}');".format(ticker, openPrice, closePrice, highPrice, lowPrice, volume, date))
-#
-#     # mariadb_connection.commit()
-#
-#     iPhoneToken = 'eed50d343790b15fb440554457873a8730420c47abd789f07aa75003508039cf'
-#
-#     token_hex = '6523c10eee317c4cd614156972904a5467e0c47708551e4c8b78e7bd7a6e2507'
-#
-#     tokens = [iPhoneToken, token_hex]
-#
-#     alertString = "New price for {0}! ${1}".format(ticker, closePrice)
-#
-#     payload = ""
-#     # payload = Payload(alert=alertString, sound="default", badge=1)
-#     topic = 'com.new.CS411Project'
-#     # client = APNsClient('apns-dev-cert.pem', use_sandbox=True, use_alternative_port=False)
-#
-#
-#     #client.send_notification(token_hex, payload, topic)
-#
-#
-#     return jsonify({'result': 'inserted'})
-
-# Delete - DELETE
-# @app.route('/<string:ticker>/<string:date>', methods=['DELETE'])
-# def deleteRecord(ticker, date):
-#     '''
-#     A function that handles our DELETE API Endpoint.
-#     More specificaly, the function is used to delete a tuple in our database.
-#
-#     - Parameters:
-#     - cid: the cid for the tuple to be deleted
-#
-#     return: status msg
-#     '''
-#     print("deleteRecord()")
-#
-#     # query the DB
-#     cursor.execute("DELETE FROM StockData WHERE ticker = '{0}' AND date = DATE '{1}';".format(ticker, date))
-#
-#     # mariadb_connection.commit()
-#
-#     return jsonify({'result': 'deleted'})
