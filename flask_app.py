b09eb089449991a4b2eeb6309e272fe1e3d868aa
Athena96 <jaredfranzone@gmail.com>
2018-12-08 08:20:54 +0000

    spending_api: added get_budget, update, and delete endpoints

--- flask_app.py
+++ flask_app.py
@@ -90,6 +90,29 @@ def add_purchase(item, price, category, date, note):
 
     return jsonify({'result': 'successfuly added purchase!'})
 
+@app.route('/<string:purchase_id>/<string:item>/<string:price>/<string:category>/<string:date>/<string:note>', methods=['PUT'])
+def update_purchase(purchase_id, item, price, category, date, note):
+    print("update_purchase()")
+    print((purchase_id, item, price, category, date, note))
+
+    # open
+    db_conn = sqlite3.connect(DATABASE)
+    cursor = db_conn.cursor()
+
+    cursor.execute(
+    """UPDATE spending SET item = '{0}', price = {1}, category = '{2}', date = '{3}', note = '{4}'
+    WHERE spending.purchase_id = {5}""".format(item,
+    price, category, date, note, purchase_id))
+
+    db_conn.commit()
+
+    # close
+    db_conn.close()
+
+    return jsonify({'result': 'successfuly updated purchase!'})
+
+
+
 @app.route('/<string:purchase_id>', methods=['DELETE'])
 def delete_purchase(purchase_id):
     print("delete_purchase(): ", purchase_id)
@@ -137,15 +160,31 @@ def get_spending_report(month, year):
     # send data
     return jsonify(categories)
 
+@app.route('/<string:budget>', methods=['GET'])
+def get_budget(budget):
+    print("get_budget()")
+    if budget != "budget":
+        return jsonify({'result': 'bad budget request'})
 
+    # open
+    db_conn = sqlite3.connect(DATABASE)
+    cursor = db_conn.cursor()
 
-# Update - PUT
-# @app.route('/<string:oldTicker>/<string:oldDate>/<string:ticker>/<string:openPrice>/<string:closePrice>/<string:highPrice>/<string:lowPrice>/<string:volume>/<string:date>', methods=['PUT'])
-# def updateRecord(oldTicker, oldDate, ticker, openPrice, closePrice, highPrice, lowPrice, volume, date):
-#     # query the DB
-#     # SELECT ticker, openPrice, closePrice, highPrice, lowPrice, volume, CAST(date AS char)
-#     cursor.execute("UPDATE StockData SET ticker = '{0}', openPrice = {1}, closePrice = {2}, highPrice = {3}, lowPrice = {4}, volume = {5}, date = DATE '{6}' WHERE ticker = '{7}' AND date = DATE '{8}';".format(ticker, openPrice, closePrice, highPrice, lowPrice, volume, date, oldTicker, oldDate))
-#
-#     # mariadb_connection.commit()
-#
-#     return jsonify({'result': 'updated'})
+    # query
+    cursor.execute("SELECT * FROM budget")
+
+    # get list of categories
+    data = []
+    for category, amount, amount_frequency, category_id in cursor:
+        contents = {}
+        contents["category"] = category
+        contents["amount"] = amount
+        contents["amount_frequency"] = amount_frequency
+        contents["category_id"] = category_id
+        data.append(contents)
+
+    # close
+    db_conn.close()
+
+    # send data
+    return jsonify(data)
