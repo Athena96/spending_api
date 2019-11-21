1211df27f6f248262d71d1a389425ececc1f6cd2
Jared Franzone <Jared.Franzone@cerner.com>
2019-11-21 16:16:42 -0600

    link to txn page from summary

--- static/js/recurrences.js
+++ static/js/recurrences.js
@@ -18,28 +18,3 @@ function deleteRecurrence(recurrence_id) {
         alert(this.responseText);
     }
 }
-
-function openPurhcasesPage(info) {
-    var parts = info.split("#");
-    var title = parts[0];
-    var category = parts[1];
-    var month = "";
-    var year = "";
-
-    var xhr = new XMLHttpRequest();
-    var url = "";
-    if (title == "month_year") {
-        month = parts[2];
-        year = parts[3];
-        url = prefix + "/site/transactions" + "/year:" + year + "/month:" + month + "/category:" + category;
-    } else if (title == "year") {
-        year = parts[2];
-        url = prefix + "/site/transactions" + "/year:" + year + "/category:" + category;
-    } else if (title == "period") {
-        var start_date = parts[1];
-        var end_date = parts[2];
-        var category = parts[3];
-        url = prefix + "/site/transactions" + "/start_date:" + start_date + "/end_date:" + end_date + "/category:" + category;
-    }
-    window.open(url);
-}
